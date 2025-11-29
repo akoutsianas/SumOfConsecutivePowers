a8c0cfd3f29387d562588e00c29a3809825bf345
Angelos Koutsianas <koutsis.jr@gmail.com>
2025-11-29 13:49:48 +0200

    Create class SumOfConsecutivePowersModularMethod.

--- modular_method/elimination_step.py
+++ modular_method/elimination_step.py
@@ -13,138 +13,128 @@ VALUES_d_d1 = {
 }
 
 
-def bound_exponent_n(k, primes_bound = 50):
-    """
-    INPUT
-        - k: an integer equivalent 2 mod 4
-        - primes_bound: an upper bound of the primes we use in the elimination step
+class SumOfConsecutivePowersModularMethod:
 
-    OUTPUT:
-        A dictionary with the rational newforms over this elimination step fails and a list of small primes the modular
-        method can not eliminate for the remaining newforms.
-    """
+    def __init__(self, k):
+        self.k = k
+        self.info = None
+        self._x = polygen(QQ, 'x')
+        self.fk = self._compute_fk()
 
-    if k % 4 != 2:
-        raise ValueError(f"k is not equal to 2 mod(4)!")
+    def _compute_fk(self):
+        x = polygen(QQ, 'x')
+        g = ((self._x - 1)**self.k + (self._x + 1)**self.k)/2
+        f = self._x.parent(g/(self._x**2 + 1))
+        f = sum([fi*x**i for i, fi in enumerate(f.coefficients())])
+        return f
 
-    Ex = lambda x: EllipticCurve([0, 2*x, 0, x**2 + 1, 0])
-    values_d_d1 = VALUES_d_d1[k]
-    info = {}
-    for d in values_d_d1.keys():
-        info[d] = {'failed_newforms': [], 'Bd': [2, 3, 5]}
-        D = prod(ZZ(d).prime_factors())
-        newfs = Newforms(2**7 * D, names="theta")
-        print(f"We apply elimination step for d={d}.")
-        for fnew in newfs:
-            is_rational = 1 if fnew.base_ring().degree == 1 else 0
-            print(f"Elimination step for newform f={fnew}")
-            Bqs = []
-            for q in prime_range(primes_bound):
-                if (2*D) % q != 0:
-                    # print(f"q: {q}")
-                    Bq = q
-                    aqf = fnew[q]
-                    for x0 in range(q):
-                        # print(f"x0: {x0}")
-                        if (x0**2 + 1) % q == 0:
-                            Bq *= aqf**2 - (q + 1)**2
+    def eliminate_newforms_method_1(self, primes_bound = 50):
+        """
+        INPUT
+            - primes_bound: an upper bound of the primes we use in the elimination step
+
+        OUTPUT:
+            Saves in info variable a dictionary with the rational newforms over this elimination step fails and a list
+            of small primes the modular method can not eliminate for the remaining newforms.
+        """
+
+        if self.k % 4 != 2:
+            raise ValueError(f"k is not equal to 2 mod(4)!")
+
+        Ex = lambda x: EllipticCurve([0, 2*x, 0, x**2 + 1, 0])
+        values_d_d1 = VALUES_d_d1[self.k]
+        info = {}
+        for d in values_d_d1.keys():
+            info[d] = {'failed_newforms': [], 'Bd': [2, 3, 5]}
+            D = prod(ZZ(d).prime_factors())
+            newfs = Newforms(2**7 * D, names="theta")
+            print(f"We apply elimination step 1 for d={d}.")
+            for newf in newfs:
+                is_rational = 1 if newf.base_ring().degree == 1 else 0
+                print(f"Elimination step for newform f={newf}")
+                Bqs = []
+                for q in prime_range(primes_bound):
+                    if (2*D) % q != 0:
+                        Bq = q
+                        aqf = newf[q]
+                        for x0 in range(q):
+                            if (x0**2 + 1) % q == 0:
+                                Bq *= aqf**2 - (q + 1)**2
+                            else:
+                                aEx0 = q + 1 - Ex(x0).reduction(q).order()
+                                Bq *= aqf - aEx0
+                        if is_rational:
+                            Bqs.append(Bq)
                         else:
-                            # print(f"Ex0: {Ex(x0)}")
-                            aEx0 = q + 1 - Ex(x0).reduction(q).order()
-                            # print(f"dif: {aqf - aEx0}")
-                            Bq *= aqf - aEx0
-                    # print(f"Bq: {Bq}")
-                    if is_rational:
-                        Bqs.append(Bq)
-                    else:
-                        Bqs.append(Bq.norm())
-                    # print(f"Bqs: {Bqs}")
-                    # print(f"------")
-            # print(f"Bqs: {Bqs}")
-            # print(f"gcd(Bqs): {gcd(Bqs)}")
-            if gcd(Bqs) != 0:
-                # print(f"small_primes: {ZZ(gcd(Bqs)).prime_factors()}")
-                for q in ZZ(gcd(Bqs)).prime_factors():
-                    if q not in info[d]['Bd']:
-                        info[d]['Bd'].append(q)
-            else:
-                info[d]['failed_newforms'].append(fnew)
-            # break
-    return info
+                            Bqs.append(Bq.norm())
+                if gcd(Bqs) != 0:
+                    for q in ZZ(gcd(Bqs)).prime_factors():
+                        if q not in info[d]['Bd']:
+                            info[d]['Bd'].append(q)
+                else:
+                    info[d]['failed_newforms'].append(newf)
+        self.info = info
 
+    def eliminate_newforms_method_2(self, bound_n, bound_t=50, lower_bound_n=7):
+        """
+        INPUT:
+            - bound_n: an upper bound of n
+            - t_bound: a bound of t such that l = t*n + 1
 
-def eliminate_exponents_range(k, info, bound_n, bound_t=50, lower_bound_n=7):
-    """
-    INPUT:
-        - k: an integer equivalent 2 mod 4
-        - info: the dictionary that we get from bound_exponent_n function
-        - Bn: an upper bound of n
-        - t_bound: a bound of t such that l = t*n + 1
+        OUTPUT:
+            A list of prime between 7 and bound_n that we are not able to eliminate using Proposition 5.3
+        """
+        if self.info is None:
+            raise ValueError(f"You have to apply the 1st elimination method!")
 
-    OUTPUT:
-        A list of prime between 7 and Bn that we are not able to eliminate using Proposition 5.3
-    """
-    fk = compute_fk(k)
-    fk = fk(fk.parent().gen()**2)
-    print(f"fk: {fk}")
-    fk /= 2**(k-2)
-    problematic_n = []
-    for n in prime_range(lower_bound_n, bound_n + 1):
-        success_n = eliminate_n(k, n, info, fk, bound_t=bound_t)
-        if not success_n:
-            problematic_n.append(n)
-        print(f"eliminate_n: {success_n}-{n}")
+        fk = self.fk(self._x**2)
+        fk /= 2**(self.k-2)
+        problematic_n = []
+        for n in prime_range(lower_bound_n, bound_n + 1):
+            success_n = self._eliminate_n(n, bound_t=bound_t)
+            if not success_n:
+                problematic_n.append(n)
+            print(f"eliminate_n: {success_n}-{n}")
 
-    return problematic_n
+        return problematic_n
 
+    def _eliminate_n(self, n, bound_t=50):
+        print(f"Exponent n: {n}")
+        for d in VALUES_d_d1[self.k].keys():
+            for d1 in VALUES_d_d1[self.k][d]:
+                for newf in self.info[d]['failed_newforms']:
+                    if not self._eliminate_n_newf(n, newf, d, d1, bound_t=bound_t):
+                        return False
+        return True
 
-def eliminate_n(k, n, info, fk, bound_t=50):
-    print(f"Exponent n: {n}")
-    for d in VALUES_d_d1[k].keys():
-        for d1 in VALUES_d_d1[k][d]:
-            for newf in info[d]['failed_newforms']:
-                if not eliminate_n_fnew(k, n, newf, d, d1, fk, bound_t=bound_t):
-                    return False
-    return True
-
-
-def eliminate_n_fnew(k, n, newf, d, d1, fk, bound_t=50):
-    Ex = lambda x: EllipticCurve([0, 2 * x, 0, x ** 2 + 1, 0])
-    for t in range(2, bound_t + 1):
-        l = ZZ(t) * n + 1
-        if (l in Primes()) and (ZZ(k / 2) % l != 0):
-            suitable_l = True
-            Fl = FiniteField(l)
-            fkbar = fk.change_ring(Fl)
-            y = polygen(Fl, 'y')
-            t_unit_roots = [r[0] for r in (y ** t - 1).roots()]
-            if l % 4 == 1:
-                t_unit_roots.append(Fl(0))
-            d2 = 1 / (Fl(d) ** 2 * Fl(d1))
-            alf = newf[l]
-            for zt in t_unit_roots:
-                x0s = [r[0] for r in (y ** 2 + 1 - 2 * Fl(d) * Fl(d1) * zt).roots()]
-                for x0 in x0s:
-                    y2 = Fl(fkbar(x0) / (Fl(d) * Fl(d2)))
-                    if y2 in t_unit_roots or y2.is_zero():
-                        aEx0 = l + 1 - Ex(x0).order()
-                        diff = (aEx0 - alf).norm()
-                        if diff % n == 0:
-                            suitable_l = False
-                            break
-                if not suitable_l:
-                    break
-            if l % 4 == 1:
-                diff = (4 - alf ** 2).norm()
-                if diff % n == 0:
-                    suitable_l = False
-            if suitable_l:
-                return True
-    return False
-
-def compute_fk(k):
-    x = polygen(QQ, 'x')
-    g = ((x - 1)**k + (x + 1)**k)/2
-    f = x.parent(g/(x**2 + 1))
-    f = sum([fi*x**i for i, fi in enumerate(f.coefficients())])
-    return f
+    def _eliminate_n_newf(self, n, newf, d, d1, bound_t=50):
+        Ex = lambda x: EllipticCurve([0, 2 * x, 0, x ** 2 + 1, 0])
+        for t in range(2, bound_t + 1):
+            l = ZZ(t) * n + 1
+            if (l in Primes()) and (ZZ(self.k / 2) % l != 0):
+                suitable_l = True
+                Fl = FiniteField(l)
+                fkbar = self.fk.change_ring(Fl)
+                y = polygen(Fl, 'y')
+                t_unit_roots = [r[0] for r in (y ** t - 1).roots()]
+                d2 = 1 / (Fl(d) ** 2 * Fl(d1))
+                alf = newf[l]
+                for zt in t_unit_roots:
+                    x0s = [r[0] for r in (y ** 2 + 1 - 2 * Fl(d) * Fl(d1) * zt).roots()]
+                    for x0 in x0s:
+                        y2 = Fl(fkbar(x0) / (Fl(d) * Fl(d2)))
+                        if y2 in t_unit_roots or y2.is_zero():
+                            aEx0 = l + 1 - Ex(x0).order()
+                            diff = (aEx0 - alf).norm()
+                            if diff % n == 0:
+                                suitable_l = False
+                                break
+                    if not suitable_l:
+                        break
+                if l % 4 == 1:
+                    diff = (4 - alf ** 2).norm()
+                    if diff % n == 0:
+                        suitable_l = False
+                if suitable_l:
+                    return True
+        return False
